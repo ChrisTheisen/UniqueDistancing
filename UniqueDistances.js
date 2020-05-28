7562c66087282867470f7d4825702a3919822c66
Chris Theisen <grumdrig333@gmail.com>
2020-05-28 10:10:32 -0500

    Add files via upload
    Made distinct Rows/Cols/Goal. 
    Made some more buttons.
    Allow user to click cells to toggle.
    Added some text.
    
    JS file is still a bit messy.

--- UniqueDistances.js
+++ UniqueDistances.js
@@ -1,13 +1,19 @@
 function point(x,y) {this.x=x||0; this.y=y||0;}
 point.prototype.toString = function(){return "("+this.x+","+this.y+")";}
-var size = 3;
-var area = size**2;
+var rows = 3;
+var cols = 3;
+var goal = 3;
+var area = rows*cols;
 var solutions = [];
 var speed = 10;
+var shouldStop = 0;
+var drawPoints = [];
 
 function drawSolution(){
 	solution = document.getElementById("solutions").value;
-	lightupJSON(solution, "#0F0");
+	if(solution){
+		lightupJSON(solution, "#0F0");
+	}
 }
 
 function lightupPoints(points, color){
@@ -17,9 +23,11 @@ function lightupPoints(points, color){
 function lightupJSON(solution, color){
 	var solution = JSON.parse(solution);
 	
+	drawPoints = [];
+	
 	for(var i=0;i<area;i++){
-		var x = i%size;
-		var y = Math.floor(i/size);
+		var x = i%cols;
+		var y = Math.floor(i/cols);
 		var id = "c"+x+"_"+y;
 		
 		document.getElementById(id).style.backgroundColor = "#333";
@@ -30,6 +38,7 @@ function lightupJSON(solution, color){
 		var y = solution[i].y;
 		var id = "c"+x+"_"+y;
 		
+		drawPoints.push(new point(x,y));
 		document.getElementById(id).style.backgroundColor = color;
 	}
 	
@@ -64,19 +73,19 @@ function isValid(points){
 }
 
 function addPoint(points, input){
-	var x = input%size;
-	var y = Math.floor(input/size);
+	var x = input%cols;
+	var y = Math.floor(input/cols);
 	
 	points.push(new point(x,y));
 }
 
 function getValueFromPoint(input){
-	return (input.y*size)+input.x
+	return (input.y*cols)+input.x
 }
 
 function findValidPoints(points, init){
 	setTimeout(function(){
-		if(init >= area){ 
+		if(init >= area || shouldStop){ 
 			let p = new point();
 			var next = area;
 			var missingPoints = area;
@@ -84,17 +93,16 @@ function findValidPoints(points, init){
 			while(next + missingPoints >= area){
 				p = points.pop();
 				
-				if(p == null){
+				if(p == null || shouldStop){
 					var end = new Date();
 					console.log("Duration:",end-start);
 					drawSolution();
-					document.getElementById("btn").style.display=null;
-					document.getElementById("solutions").disabled = false;
+					enable();
 					return;
 				}
 				
 				next = getValueFromPoint(p);
-				missingPoints = size - points.length;
+				missingPoints = goal - points.length;
 			}
 			init = next + 1;
 		}
@@ -105,19 +113,56 @@ function findValidPoints(points, init){
 			if(speed>0){lightupPoints(points, "#F00");}
 			points.pop();
 		}
-		else if(points.length == size){
+		else if(points.length >= goal){
 			lightupPoints(points, "#0F0");
 			foundSolution(points);
 			points.pop();
 		}
 		else{
-			if(speed>0){lightupPoints(points, "#0F0");}
+			if(speed>0){lightupPoints(points, "#00F");}
 		}
 		
 		findValidPoints(points, init+1);
 	}, speed);
 }
 
+function togglePoint(input){
+	var p = new point(input.getAttribute("col")*1, input.getAttribute("row")*1);
+
+	var exists = drawPoints.find(e => e.x == p.x && e.y == p.y);
+	console.log(input, p, drawPoints, exists);
+	if(!exists){
+		if(drawPoints.length >= goal){return;}
+		drawPoints.push(p);
+	}
+	else{
+		var index = drawPoints.indexOf(exists);
+		drawPoints.splice(index, 1);
+	}
+
+	if(!isValid(drawPoints)){
+		lightupPoints(drawPoints, "#F00");
+	}
+	else if(drawPoints.length >= goal){
+		lightupPoints(drawPoints, "#0F0");
+	}
+	else{
+		lightupPoints(drawPoints, "#00F");
+	}
+	
+}
+
+
+
+function generateBoard(){
+	rows = document.getElementById("rows").value*1;
+	cols = document.getElementById("cols").value*1;
+	area = rows*cols;
+	goal = document.getElementById("goal").value*1;
+
+	buildBoard(rows, cols);
+
+}
 function buildBoard(rows, cols){
 	
 	var b = document.getElementById("board");
@@ -133,9 +178,30 @@ function buildBoard(rows, cols){
 			var c = document.createElement("div");
 			c.classList.add("cell");
 			c.id = "c" + i + "_" + j;
+			c.onclick = function(){ togglePoint(this); };
+			c.setAttribute("col", i);
+			c.setAttribute("row", j);
 			col.appendChild(c);
 		}
 	}
+}
+
+function disable(){
+	document.getElementById("btn").disabled = true;
+	document.getElementById("solutions").disabled = true;
+	document.getElementById("rows").disabled = true;
+	document.getElementById("cols").disabled = true;
+	document.getElementById("speed").disabled = true
+	document.getElementById("stop").disabled = false;
+}
+
+function enable(){
+	document.getElementById("btn").disabled = false;
+	document.getElementById("solutions").disabled = false;
+	document.getElementById("rows").disabled = false;
+	document.getElementById("cols").disabled = false;
+	document.getElementById("speed").disabled = false
+	document.getElementById("stop").disabled = true;
 }
 
 function removeOptions() {
@@ -147,21 +213,19 @@ function removeOptions() {
 }
 
 var start = new Date();
-function main(){
+function GO(){
 	speed = (document.getElementById("speed").value**2)*10;
 	start = new Date();
 	console.log(start);
 	removeOptions();
-	document.getElementById("btn").style.display= "none";
-	document.getElementById("solutions").disabled = true;
 
-	size = document.getElementById("size").value;
-	area = size**2;
+	generateBoard();
 
-	buildBoard(size, size);
+	disable();
+	shouldStop = 0;
 	findValidPoints([], 0);
 }
 
-
+function stop(){shouldStop = 1;}
 
 
